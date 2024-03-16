0c0b4ec39887a6c179984c1ee1caf59bd9328364
RanaHisham18 <hishamrana00@gmail.com>
2024-03-17 01:26:58 +0200

    setUp Spinner

--- app/src/main/java/com/example/mydropdownapplication/MainActivity.kt
+++ app/src/main/java/com/example/mydropdownapplication/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.mydropdownapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.mydropdownapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupSpinner(binding.spinner1, R.array.spinner1_items)
+        setupSpinner(binding.spinner2, R.array.spinner2_items)
     }
-}
+
+    private fun setupSpinner(spinner: Spinner, spinnerItems: Int) {
+        ArrayAdapter.createFromResource(
+            this,
+            spinnerItems,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+
+        }
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                // Handle selection
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle the absence of selection
+            }
+    }
+} }
